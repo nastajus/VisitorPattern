e79430839a98b1c5ad6150316f03cbf6590f091f
Ian Nastajus <ian.nastajus@gmail.com>
2016-08-20 21:38:36 -0400

    basic prototype works, where A invokes B only. Inspired by Visitor pattern, not 100% certain is truly following that pattern
    also seems there's easier way to accomplish this pattern in C#, my example was Java-based.

--- Assets/Thing.cs
+++ Assets/Thing.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+//it seems inheriently wrong to invoke Accept from within Visit... but I cannot prove nor disprove this easily.
+//however in this case since the "Thing" class is designed to be both the Visitor + Visitable, it may actually make sense
+//thus I want to conditionally control execution of the Accept call here
+//if A is arriving, invoke
+
 public class Thing : MonoBehaviour, Visitable, Visitor {
 
-	bool isColliding = false;
+	int health = 10;
+
+	bool isColliding = false; // required to limit multiple executions for a single OnCollisionEnter to once per instance
 
 	void Start () {
 	
@@ -19,9 +27,15 @@ public class Thing : MonoBehaviour, Visitable, Visitor {
 		isColliding = true;
 
 		print("col.gameObject.name: " + col.gameObject.name + ", this.gameObject.name: " + this.gameObject.name);
-
-		Visitor visitor = col.gameObject.GetComponent<Visitor>();
-		visitor.Visit(this);
+		
+		//conditional acceptance, only when A visits B, not vice-versa
+		A a = col.gameObject.GetComponent<A>();
+		if (a)
+		{
+			Visitor visitor = col.gameObject.GetComponent<Visitor>();
+			Visitable visitable = gameObject.GetComponent<Visitable>();
+			visitable.Accept(visitor);
+		}
 	}
 
 	void OnCollisionExit(Collision col)
@@ -31,12 +45,22 @@ public class Thing : MonoBehaviour, Visitable, Visitor {
 
 	public void Visit(Visitable visitiable)
 	{
-		Accept((Visitor)visitiable);
-		print("visitiable called by: " + visitiable);
+		print(visitiable + " visits " + this + ", but so what?" );
+		ReduceHealthBWhenAinteracts();
 	}
 
 	public void Accept(Visitor visitor)
 	{
-		print("accept called by: " + visitor);
+		print(visitor + " accepts " + this);
+		visitor.Visit(this);
 	}
+
+	//goal is to only trigger this method from A colliding with B, and not vice-versa
+	//ideally in a manner that seems clean
+	private void ReduceHealthBWhenAinteracts()
+	{
+		health -= 3;
+		print("health of : " + this + " is: " + health);
+	}
+
 }
